4d654dc826cf9e0ac20763e323faf91e6f874ff3
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2020-05-11 11:28:11 +0200

    Update SHA1 certificate to use and its CA.

--- codeSigning/sign.bat
+++ codeSigning/sign.bat
@@ -1,7 +1,7 @@
 PATH=%PATH%;%WSDK81%\bin\x86
 
 rem sign using SHA-1
-signtool sign /v /sha1 02903e55b5b7877154ac9f2de48f64b00ca3d755 /ac DigiCert_High_Assurance_Code_Signing_CA.cer /fd sha1 /t http://timestamp.verisign.com/scripts/timestamp.dll "..\bin\entropyCalc.exe" "..\bin\entropyCalc-x64.exe" "..\bin\entropyCalc-arm64.exe" "..\bin\entropyCalc-arm.exe"
+signtool sign /v /sha1 85AA2E55CFB9C38FE474C58B38E9521450CD9306 /ac DigiCert_Assured_ID_Code_Signing_CA.cer /fd sha1 /t http://timestamp.digicert.com "..\bin\entropyCalc.exe" "..\bin\entropyCalc-x64.exe" "..\bin\entropyCalc-arm64.exe" "..\bin\entropyCalc-arm.exe"
 rem sign using SHA-256
 signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /as /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 "..\bin\entropyCalc.exe" "..\bin\entropyCalc-x64.exe" "..\bin\entropyCalc-arm64.exe" "..\bin\entropyCalc-arm.exe"
 
